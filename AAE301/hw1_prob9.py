898e87988c444a384789d7204ff00d3f3eca98e9
brucelabounty <blabount@purdue.edu>
2025-09-02 21:19:43 -0400

    add

---
+++ AAE301/hw1_prob9.py
@@ -0,0 +1,26 @@
+# AAE 301 (Signals I)
+# Problem 9: Plotting Parametric Curves
+# Author: Bruce LaBounty
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta = np.linspace(0, 2 * np.pi, 100) # Calculating angles
+eq1 = np.exp(1j * 3 * theta) # Equation 1
+eq2 = 1j * np.exp(1j * -2 * theta) # Equation 2
+
+plt.figure(1)
+plt.plot(np.real(eq1), np.imag(eq1), '*')
+plt.xlabel("Re")
+plt.ylabel("Im")
+plt.title("Plotting roots of Equation 1")
+plt.grid(True)
+plt.show()
+
+plt.figure(2)
+plt.plot(np.real(eq2), np.imag(eq2), '*')
+plt.xlabel("Re")
+plt.ylabel("Im")
+plt.title("Plotting roots of Equation 2")
+plt.grid(True)
+plt.show()
