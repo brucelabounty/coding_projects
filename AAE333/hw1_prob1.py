b91ad6c38210b112145c71debabc6b20ed380990
brucelabounty <blabount@purdue.edu>
2025-09-12 12:18:43 -0400

    add

--- AAE333/hw1_prob1.py
+++ AAE333/hw1_prob1.py
@@ -6,17 +6,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-char_length = 4 * 100 # Characteristic Length (cm)
+char_length = 3.5 # Characteristic Length (m)
 
 # Extracting data from file
 data = np.loadtxt(r"AAE333\nrlmsise_Dragon.txt")
 
 height = data[:, 0] # Altitude (km)
-N2 = data[:, 1] # Number Density for diatomic Nitrogen (cm-3)
-O2 = data[:, 2] # Number Density for diatomic Oxygen (cm-3)
+N2 = data[:, 1] * 10**6 # Number Density for diatomic Nitrogen (m-3)
+O2 = data[:, 2] * 10**6 # Number Density for diatomic Oxygen (m-3)
 
 # Calculating Mean Free Path (MFP)
-d_N2 = 36e-7 # Diameter of diatomic Nitrogen (cm)
+d_N2 = .36e-9 # Diameter of diatomic Nitrogen (m)
 
 mfp = 1 / (np.sqrt(2) * np.pi * (d_N2)**2 * (N2 + O2))
 
@@ -29,3 +29,8 @@ plt.ylabel("Knudsen Number")
 plt.title("Knudsen Number versus Height [km]")
 plt.grid(True)
 plt.show()
+
+# Finding the value where flow switches to continuum
+idx = np.where(Kn < 0.01)
+cont_alt = height[idx]
+print(cont_alt[-1])
