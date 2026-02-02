01e99f83a5bfa17b107d2e4b0a3aabda5b2871c4
brucelabounty <blabount@purdue.edu>
2026-02-02 11:00:49 -0500

    add

---
+++ AAE334/aae334_hw3_prob2.py
@@ -0,0 +1,27 @@
+# AAE 334 Homework 3 Problem 2 (Using Python)
+# Author: Bruce LaBounty
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Initializing constants (from part a)
+a1 = 0.4189
+a2 = 0.5585
+
+x = np.linspace(0,1,500)
+
+# Equations for Camber Line equation
+eq1 = a1 * (x - x**2)
+eq2 = a2 * (x - x**2) * (1 - 2*x)
+
+camb_line = eq1 + eq2 # Camber Line Equation
+
+# Plotting
+plt.plot(x, camb_line)
+plt.axis('equal')
+plt.grid(True)
+plt.xlim(0,1)
+plt.title("Camber Line Profile")
+plt.xlabel("x-axis")
+plt.ylabel("z-axis")
+plt.show()
